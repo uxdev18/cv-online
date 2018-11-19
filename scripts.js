486354e1ce319f9b2eaa21cc4aebfb5982aa25be
kate <gdakaska@gmail.com>
2018-11-19 20:44:40 +0100

    Tidy up

--- scripts.js
+++
@@ -1,9 +0,0 @@
-$('#change').on('click', function() {
-    $('div').each(function() {
-        if($(this).hasClass('active')) {
-            $(this).removeClass('active');
-        } else {
-            $(this).addClass('active');
-        }
-    });
-});
